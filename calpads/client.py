f6bcd13d6c26eb047de4a6011c90ef50d832f325
Yusuph Mkangara <yusuph.mka@outlook.com>
2020-05-23 21:50:16 -0700

    Functional v1 reports download. Un-Real.

--- calpads/client.py
+++ calpads/client.py
@@ -1,8 +1,11 @@
 import requests
 import logging
 import json
-from urllib.parse import urlencode, urlsplit, urlunsplit, parse_qs, urljoin
+import re
+from urllib.parse import urlencode, urlsplit, urlunsplit, parse_qsl, urljoin
+from collections import deque
 from bs4 import BeautifulSoup, Tag
+from lxml import etree
 
 
 class CALPADSClient:
@@ -13,6 +16,7 @@ class CALPADSClient:
         self.password = password
         self.credentials = {'Username': self.username,
                             'Password': self.password}
+        self.visit_history = deque(maxlen=10)
         self.session = requests.Session()
         self.session.headers.update({'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
         (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
@@ -26,17 +30,15 @@ class CALPADSClient:
             self.__connection_status = self.login()
         except RecursionError:
             self.log.info("Looks like the provided credentials might be incorrect. Confirm credentials.")
-        #TODO: Find out what error happens when CALPADS is closed for nightly process.
+
 
     def login(self):
         #Dance the OAuth Dance
         self.session.get(self.host)
-        #If you hit every beat, fetching the host again should return the host url
-        homepage_response = self.session.get(self.host)
         # self.log.debug(homepage_response.url)
         # self.log.debug(self.session.cookies)
         # self.log.debug(self.session.get(self.host + 'Leas?format=JSON').content) # Easy check if logging in happened
-        return homepage_response.ok and homepage_response.url == self.host
+        return self.visit_history[-1].status_code == 200 and self.visit_history[-1].url == self.host
 
     @property
     def is_connected(self):
@@ -180,6 +182,93 @@ class CALPADSClient:
         response = self.session.get(urljoin(self.host, f'/Student/{ssid}/PSTS?format=JSON'))
         return json.loads(response.content)
 
+    def download_ods_report(self, report_code, file_name):
+        with self.session as session:
+            report_url = self._get_report_link(report_code.lower())
+            if report_url:
+                session.get(report_url)
+            else:
+                raise Exception("Report Not Found")
+            iframe_url = BeautifulSoup(self.visit_history[-1].text, parser="lxml").find('iframe').get('src')
+            session.get(iframe_url)
+            all_with_names = BeautifulSoup(self.visit_history[-1].text, parser="lxml").find_all(lambda x: x.has_attr('name'))
+
+            form_inputs_to_keep = ['__VIEWSTATE', '__VIEWSTATEGENERATOR', '__EVENTVALIDATION']
+            form_inputs_endings = ('HiddenIndices', 'txtValue', 'ddValue')
+
+            in_expected_keys = [tag for tag in all_with_names if tag['name'] in form_inputs_to_keep
+                                or tag['name'].endswith(form_inputs_endings)]
+            in_expected_keys_names = [tag['name'] for tag in in_expected_keys]
+            values_in_expected_key_tags = [tag.get('value', '') for tag in in_expected_keys]
+            form_data = dict(zip(in_expected_keys_names, values_in_expected_key_tags))
+            prefilled_values = {"ReportViewer1$ctl08$ctl03$ddValue": 3,
+                                "ReportViewer1$ctl08$ctl05$ddValue": 5,
+                                "ReportViewer1$ctl08$ctl07$ddValue": 19,
+                                "ReportViewer1$ctl08$ctl09$ddValue": 1,
+                                "ReportViewer1$ctl08$ctl23$ddValue": 1,
+                                "ReportViewer1$ctl08$ctl11$divDropDown$ctl01$HiddenIndices": '0,1',
+                                "ReportViewer1$ctl08$ctl13$divDropDown$ctl01$HiddenIndices": '2',
+                                "ReportViewer1$ctl08$ctl15$divDropDown$ctl01$HiddenIndices": '5,6,7',
+                                "ReportViewer1$ctl08$ctl17$divDropDown$ctl01$HiddenIndices": '0',
+                                "ReportViewer1$ctl08$ctl19$divDropDown$ctl01$HiddenIndices": '0,1',
+                                "ReportViewer1$ctl08$ctl21$divDropDown$ctl01$HiddenIndices": '0,1,2,3'
+                                }
+            form_data.update(prefilled_values)
+
+            # TODO: Test how form data treats None or False diferently from empty string
+            form_data = {k: v for k, v in form_data.items() if v != ''}
+
+            session.post(self.visit_history[-1].url,
+                         data=form_data)
+
+            regex = re.compile('(?<="ExportUrlBase":")[^"]+(?=")')  # Look for text sandwitched between the lookbehind and
+            # the lookahead, but EXCLUDE the double quotes (i.e. find the first double quotes as the upper limit of the text)
+
+            split_query = None
+            if regex.search(self.visit_history[-1].text):
+                self.log.debug('Found ExportUrlBase in the URL')
+                export_url_base = regex.search(self.visit_history[-1].text).group(0)
+                q = urlsplit(urljoin("https://reports.calpads.org",
+                                     export_url_base)
+                             .replace('\\u0026', '&')
+                             .replace('%3a', ':')
+                             .replace('%2f', '/')
+                             ).query
+                split_query = parse_qsl(q)
+
+            if split_query:
+                self.log.debug("Adding Format parameter to the URL")
+                split_query.append(('Format', "PDF"))
+                self.log.debug("Rejoining the query elements again again")
+                scheme, netloc, path, query, frag = urlsplit(urljoin("https://reports.calpads.org",
+                                                                     export_url_base))
+
+                report_dl_url = urlunsplit([scheme, netloc, path, urlencode(split_query), frag])
+                session.get(report_dl_url)
+                with open(file_name, 'wb') as f:
+                    f.write(self.visit_history[-1].content)
+                    return "We did that yo!"
+
+    def _get_report_link(self, report_code, is_snapshot=False):
+        #TODO: Lowercase report_code either here or in whatever ends up calling it
+        with self.session as session:
+            if is_snapshot:
+                session.get('https://www.calpads.org/Report/Snapshot')
+            else:
+                session.get('https://www.calpads.org/Report/ODS')
+            response = self.visit_history[-1]
+            if report_code == '8.1eoy3' and is_snapshot:
+                # TODO: Might add another variable and if-condition to re-use for ODS as well as Snapshot
+                return 'https://www.calpads.org/Report/Snapshot/8_1_StudentProfileList_EOY3_'
+            else:
+                root = etree.fromstring(response.text, parser=etree.HTMLParser(encoding='utf8'))
+                elements = root.xpath("//*[@class='num-wrap-in']")
+                for element in elements:
+                    if report_code == element.text:
+                        return urljoin(self.host, element.xpath('./../../a')[0].attrib['href'])
+                #TODO: Write this exception in an exceptions.py
+                #raise ReportNotFound('{} report code cannot be found on the webpage'.format(report_code))
+
     def _handle_event_hooks(self, r, *args, **kwargs):
         self.log.debug(("Response STATUS CODE: {}\nChecking hooks for: \n{}\n"
                         .format(r.status_code, r.url)
@@ -228,6 +317,7 @@ class CALPADSClient:
                                   )
         else:
             self.log.debug("No response hook needed for: {}\n".format(r.url))
+            self.visit_history.append(r)
             return r
 
 
